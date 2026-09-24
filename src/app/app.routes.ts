b5713800b95864e49bb423338301f3a98f22ceca
Milly56 <jamilyalves353@gmail.com>
2026-09-24 16:13:37 -0300

    feat: feito o checkout de validação e instalei uma biblioteca.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
+
 import { ProdutoLista } from './component/produto-lista/produto-lista';
+import { Checkout } from './component/checkout/checkout';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: ProdutoLista
-    }
+
+  {
+    path: '',
+    component: ProdutoLista
+  },
+
+  {
+    path: 'checkout',
+    component: Checkout
+  }
+
 ];
