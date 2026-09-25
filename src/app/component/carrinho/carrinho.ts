de1e26630c8930f1b0f437d97149fe2baf8a15c7
majuliah <majulialcarvalho@gmail.com>
2026-09-25 19:29:03 -0300

    feat: adiciona controle de quantidade e total ao carrinho

--- src/app/component/carrinho/carrinho.ts
+++ src/app/component/carrinho/carrinho.ts
@@ -11,10 +11,28 @@ import { Carrinho as CarrinhoService } from '../../services/carrinho';
 })
 export class Carrinho implements OnInit {
   itens: any[] = [];
+  total = 0;
 
   constructor(private carrinhoService: CarrinhoService) {}
 
   ngOnInit(): void {
     this.itens = this.carrinhoService.listar();
+    this.total = this.carrinhoService.calcularTotal();
+  }
+
+  aumentarQuantidade(id: number): void {
+    this.carrinhoService.aumentarQuantidade(id);
+    this.total = this.carrinhoService.calcularTotal();
+  }
+
+  diminuirQuantidade(id: number): void {
+    this.carrinhoService.diminuirQuantidade(id);
+    this.total = this.carrinhoService.calcularTotal();
+  }
+
+  remover(id: number): void {
+    this.carrinhoService.remover(id);
+    this.itens = this.carrinhoService.listar();
+    this.total = this.carrinhoService.calcularTotal();
   }
 }
--- src/app/services/carrinho.ts
+++ src/app/services/carrinho.ts
@@ -7,10 +7,46 @@ export class Carrinho {
   private itens: any[] = [];
 
   adicionar(produto: any): void {
-    this.itens.push(produto);
+    const itemExistente = this.itens.find(item => item.id === produto.id);
+
+    if (itemExistente) {
+      itemExistente.quantidade += 1;
+    } else {
+      this.itens.push({
+        ...produto,
+        quantidade: 1
+      });
+    }
+  }
+
+  aumentarQuantidade(id: number): void {
+    const item = this.itens.find(item => item.id === id);
+
+    if (item) {
+      item.quantidade += 1;
+    }
+  }
+
+  diminuirQuantidade(id: number): void {
+    const item = this.itens.find(item => item.id === id);
+
+    if (item && item.quantidade > 1) {
+      item.quantidade -= 1;
+    }
+  }
+
+  remover(id: number): void {
+    this.itens = this.itens.filter(item => item.id !== id);
   }
 
   listar(): any[] {
     return this.itens;
+  }
+
+  calcularTotal(): number {
+    return this.itens.reduce(
+      (total, item) => total + item.price * item.quantidade,
+      0
+    );
   }
 }
